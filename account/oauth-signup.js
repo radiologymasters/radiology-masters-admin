36713f92f744bdbab4dee3b7c9d74b3a389a27f8
Jamie Mckniff <jmckniff@gmail.com>
2017-11-30 22:30:22 +0000

    Fixed requirejs paths on signup page

--- account/oauth-signup.js
+++ account/oauth-signup.js
@@ -1,12 +1,12 @@
 requirejs.config({
     paths: {
         // VENDOR
-        jquery: "/node_modules/jquery/dist/jquery.min",
+        jquery: "../node_modules/jquery/dist/jquery.min",
         // INTERNAL
-        settings: '/shared/settings',
-        utils: '/shared/utils',
-        UserModel: "/shared/user.model",
-        SignupOAuthView: "/account/signup.oauth"
+        settings: '../shared/settings',
+        utils: '../shared/utils',
+        UserModel: "../shared/user.model",
+        SignupOAuthView: "signup.oauth"
     }
 });
 
