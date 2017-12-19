7084df70f343ebadc405fef7344a14b8b204fca0
Jamie Mckniff <jmckniff@gmail.com>
2017-12-19 13:34:13 +0000

    Updated vimeo access tokens

--- shared/settings.js
+++ shared/settings.js
@@ -8,10 +8,10 @@ define("settings", function() {
         updateCaseUrl: "#/cases/update/",
         deleteCaseUrl: "#/cases/delete/",
         
-        vimeoClientId: "d579942334f6fd7477d0c546d6887c89c3f08a50",
-        vimeoClientSecret: "gHw8DsnLyZ+1BKr9SBJqJPZgBYLO6OEaTDxWcvuLZw+yvZGyBnVwshpCZtdzN0TgS60fWqrKf9zSQjCvMsO8lXTYoryR/tjjQ8crfPqk2zRMEERQ1AWLf0CgxEj8T+lh",
+        vimeoClientId: "0ad8cfad61144eb7ace74df7d1ed9b3780abd2a8",
+        vimeoClientSecret: "KVW7EiyQRXqqAy8aF8vGmZzcAtGw3pR2e2vmTRjd7uaEn6qHO9mZx2qHHM2OKtwoGJcC5+vS+YklrDgWU6KVt6nNWW34YKUfbHmtyX7d31Mb8w3ZvXO6tAJAPnm1qEf2",
         // The Vimeo access token must have the delete, create and upload scopes.
-        vimeoAccessToken: "b45a33ccf5a92da4f05da80e77c531f8",
+        vimeoAccessToken: "5f82fbe07a5a1bf1828f37a7f464d90e",
         // This needs to be stored in reverse otherwise GitHub will remove it each time it is checked in.
         gitHubAccessTokenReversed: "91700385406048c11c03c2d4fef4d5bd01b7165a",
         gitHubUsername: "radiologymasters",
